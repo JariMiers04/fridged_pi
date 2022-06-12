34be02c06c18389a1880a167910923c612d831f1
JariMiers04 <jari.miers@student.ehb.be>
2022-06-12 11:59:38 +0200

    added delete in class food

--- food.py
+++ food.py
@@ -1,30 +1,17 @@
+import os
 import requests
+from os.path import dirname, abspath
+from dotenv import load_dotenv
 
-url = 'http://192.168.0.116:80/food'
+
 headers = {"Content-Type": "application/json; charset=utf-8"}
 
-testData = {
-    "expiration_date": "2022-06-08",
-    "weight": 250,
-    "short_name":"ZW COCKT WORSTJES 210G",
-	"long_name":"ZWAN coctailworstjes 210g",
-	"segment_name":"Worsten Conserv",
-	"nutriscore_label":"D",
-	"barcode":"00000025190359",
-	"image":"https:\/\/static.colruyt.be\/images\/200x310\/std.lang.all\/31\/28\/asset-1463128.jpg"
-}
-
-sendPost = requests.post(url, headers=headers, json=testData)
+path = dirname(abspath(__file__)) + '/.env'
+load_dotenv(path)
+ 
+envUrl = os.getenv('url')
 
 class FOOD(object):
-    expiration_date = ""
-    weight = 0
-    short_name = ""
-    long_name=""
-    nutriscore_label=""
-    segment_name=""
-    barcode=""
-    image=""
 
     def __init__(self, expiration_date, weight, short_name, long_name, nutriscore_label, segment_name, barcode, image):
         self.expiration_date = expiration_date
@@ -35,16 +22,28 @@ class FOOD(object):
         self.segment_name = segment_name
         self.barcode = barcode
         self.image = image
-        print(self)
 
-    def make_food(expiration_date, weight, short_name, long_name, nutriscore_label, segment_name, barcode, image):
-        food = FOOD()
-        food.expiration_date = expiration_date
-        food.weight = weight
-        food.short_name = short_name
-        food.long_name = long_name
-        food.nutriscore_label = nutriscore_label
-        food.segment_name = segment_name
-        food.barcode = barcode
-        food.image = image
-        return food
+    def PostFood(food):
+        postUrl = envUrl + "/food"
+
+        data = {
+            'expiration_date': food.expiration_date,
+            'weight': food.weight,
+            'short_name': food.short_name,
+            'long_name': food.long_name,
+            'segment_name': food.segment_name,
+            'barcode': food.barcode,
+            'image': food.image
+        }
+        print(data)
+
+        sendPost = requests.post(postUrl, headers=headers, json=data)
+
+        return sendPost
+    def DeleteFood(food):
+        deleteUrl = envUrl + "/food/" + food.barcode
+
+
+        deletePost = requests.delete(deleteUrl)
+
+        return deletePost
